2ef9056a2ff3f45a16d30483ce5a0555a603cd5d
Andrew <andrew.burrell@uts.edu.au>
2020-02-05 17:31:16 +1100

    added mlAgents 0.13

---
+++ Assets/-project/scripts/instantiateLocationCheck.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instantiateLocationCheck : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int layerMask = 1 << 8;
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, 480.0f / 2, layerMask);
+
+        if (hitColliders.Length > 1)
+        {
+            print("destroying: " + hitColliders[0].transform.name);
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void check()
+    {
+
+    }
+}
