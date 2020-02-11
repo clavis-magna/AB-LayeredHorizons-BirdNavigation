8da8e15d475cb84bd17830dfb15d8a6f47b922c3
Andrew <andrew.burrell@uts.edu.au>
2020-02-11 15:45:16 +1100

    birds flying out in 360 radius at 1:100 scale

---
+++ Assets/-project/scripts/instantiateBirds.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instantiateBirds : MonoBehaviour
+{
+    public GameObject bird;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int x = 0; x < 360; x = x + 15)
+        {
+            float angle = x + Random.Range(0, 15);
+            float outSpeed = Random.Range(5.5f, 11.11f);
+            float backSpeed = Random.Range(11.11f, 22.22f);
+            GameObject newBird = Instantiate(bird, transform.position, Quaternion.identity);
+            movementTest movementTestScript = newBird.GetComponent<movementTest>();
+            movementTestScript.angleToFly = angle;
+            movementTestScript.distancePerSecondOut = outSpeed;
+            movementTestScript.distancePerSecondBack = backSpeed;
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/-project/scripts/movementTest.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementTest : MonoBehaviour
+{
+    /*
+     * OUTGOING SPEEDS
+     * between 20 & 40 km / hour
+     * 20 km / hour scales 1:100 @ 200 meters / 36 seconds
+     * = 5.55m / second
+     * 40 km / hour scales 1:100 @ 400 meters / 36 seconds
+     * = 11.11m / second
+     * 
+     * RETURNING SPEEDS
+     * between 40 & 80 km / hour 
+     * 40 km / hour scales 1:100 @ 400 meters / 36 seconds
+     * = 11.11m / second     
+     * 80 km / hour scales 1:100 @ 800 meters / 36 seconds
+     * = 22.22m / second
+    */
+
+    float time = 1f; //seconds
+    public float distancePerSecondOut = 11.11f;
+    public float distancePerSecondBack = 22.22f;
+
+    bool isFlyingOut = true;
+    bool isFlyingHome = false;
+    Vector3 startPos;
+
+    public float angleToFly = 0;
+
+    void Start()
+    {
+        transform.rotation = Quaternion.Euler(0, angleToFly, 0);
+        //set startpos
+        startPos = transform.position;
+
+        // fly out for x amount of hours
+        Invoke("timesToFlyOut", scaleTime(3));
+
+        print("starting flight out");
+    }
+
+    void Update()
+    {
+        if (isFlyingOut)
+        {
+            transform.Translate(Vector3.forward * (Time.deltaTime * (distancePerSecondOut / time)));
+        }
+
+        if (isFlyingHome)
+        {
+            transform.Translate(Vector3.forward * (Time.deltaTime * (distancePerSecondBack / time)));
+            if(Vector3.Distance(transform.position, startPos) < 1)
+            {
+                isFlyingHome = false;
+            }
+        }
+    }
+
+    void timesToFlyOut()
+    {
+        print("staring to forage");
+        isFlyingOut = false;
+
+        // currently just waiting in the one place
+        // but will build in some foraging
+
+        // hang out (forage) for x amount of hours
+        Invoke("endForage", scaleTime(5));
+    }
+
+    void endForage()
+    {
+        print("should retuurn home now");
+        transform.LookAt(startPos);
+        isFlyingHome = true;
+    }
+
+    float scaleTime(float hoursIn)
+    {
+        //hours * 60 minutes * 60 seconds / 100
+        return ((hoursIn * 60 * 60) / 100);
+    }
+
+}
